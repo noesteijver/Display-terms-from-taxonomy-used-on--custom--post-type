5bd8356903873c876de18cd60ab227e3a9ccff7d
NOESTE IJVER <kevin@noesteijver.nl>
2015-02-11 23:35:05 +0100

    Unnecassery styling deleted, comments added

--- code.php
+++ code.php
@@ -2,6 +2,7 @@
 
 //Code to display terms from certain taxonomy (taxonomy can be set)
 //Displays terms that are used on certain (custom) post type (post type can be set)
+//To see which terms are used on a post type, we need the $wp_query
 
 ?>
 
@@ -18,18 +19,12 @@ $my_posts     = $wp_query->posts;
 $my_post_ids  = wp_list_pluck( $my_posts, 'ID' );
 $my_terms     = wp_get_object_terms( $my_post_ids, '[your taxonomy]' ); //[your taxonomy] can be 'category' or any other custom taxonomy
 
-$count      = 1;
-$number     = count($my_terms);
-
 foreach ( $my_terms as $my_term ) :
-  echo '<a href="' . get_category_link( $my_term->term_id ) . '">' . $my_term->name . '</a>';
+  
+  	echo '<a href="' . get_category_link( $my_term->term_id ) . '">' . $my_term->name . '</a>';
       
-  if($number > $count):
-    echo ' | ';
-  endif;
-  $count++;
-
 endforeach;
+
 wp_reset_query();
 
 ?>
